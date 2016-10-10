bdac5ceef91892efd14859c12a422f48527c5c7d
James Murphy <james.murphy@salecycle.com>
2016-10-10 14:42:28 +0100

    start and stop the mock server in cucumber steps

--- features/step_definitions/mock_server_steps.js
+++ features/step_definitions/mock_server_steps.js
@@ -5,10 +5,13 @@ const server = require('mockserver-grunt');
 const mockServerSteps = function() {
   this.Given(/^I have started the mock server$/, function () {
     // Write code here that turns the phrase above into concrete actions
-    return Promise.resolve('pending');
+    return server.start_mockserver({
+                serverPort: 1080,
+                verbose: true
+            });
   });
 
-  this.Given(/^I have defined a valid request$/, function (callback) {
+  this.Given(/^I have defined a valid request$/, function () {
     // Write code here that turns the phrase above into concrete actions
     return Promise.resolve('pending');
   });
@@ -18,9 +21,17 @@ const mockServerSteps = function() {
     return Promise.resolve('pending');
   });
 
-  this.Then(/^I get the expected response$/, function (callback) {
+  this.Then(/^I get the expected response$/, function () {
     // Write code here that turns the phrase above into concrete actions
     return Promise.resolve('pending');
+  });
+
+  this.Then(/^I can kill the server$/, function () {
+    // Write code here that turns the phrase above into concrete actions
+    return server.stop_mockserver({
+                serverPort: 1080,
+                verbose: true
+            });
   });
 };
 
