2658c1925d8c546d38fd82274b43750544e1e461
James Murphy <james.murphy@salecycle.com>
2016-10-11 09:45:39 +0100

    Add test for expected response using Approvals

--- features/step_definitions/mock_server_steps.js
+++ features/step_definitions/mock_server_steps.js
@@ -1,7 +1,9 @@
 'use strict'
 
 const mockServer = require('mockserver-client');
-const request = require('request-promise-native')
+const request = require('request-promise-native');
+const approvals = require('approvals');
+const path = require('path');
 
 let result;
 
@@ -14,15 +16,17 @@ const mockServerSteps = function() {
 
   this.When(/^I make the request$/, function () {
     // return request('http://localhost:1080/hw')
-    return request({uri: 'http://localhost:1080/hw', resolveWithFullResponse: true }) 
+    // To get the full response you go here
+    return request({uri: 'http://localhost:1080/hw', resolveWithFullResponse: true })
       .then((resp) => {
         result = resp;
       });
   });
 
   this.Then(/^I get the expected response$/, function () {
-    console.log('Result: ', result);
-    return Promise.resolve();
+    console.log('Status: ', result.statusCode);
+    console.log('Result: ', result.body);
+    approvals.verify(path.join(__dirname, 'approvals'), 'I_get_the_expected_response', result.body);
   });
 };
 
